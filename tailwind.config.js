20d4629a69e31904bb5291cbd24394ef48363574
Jerome <jerome.bourscheid@gmail.com>
2023-12-06 23:53:05 +0100

    <style> add header + hero + searchbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        anton: 'Anton',
+      },
+      colors: {
+        dark: '#1b1b1b',
+        darkgrey: '#7A7A7A',
+        bggrey: '#EDEDED',
+        lightgrey: '#C6C6C6',
+        primary: '#FFD15B',
+      },
+    },
   },
   plugins: [],
 };
